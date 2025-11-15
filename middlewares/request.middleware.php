f02c6fe5ad9c7fd8541205da3823acb33e00540a
aalaka <abdulquadrialaka@gmail.com>
2025-11-15 23:27:20 +1300

    added /login endpoint and its dependencies such as LoginRequest and RegisterRequest classes. Also, modified the middleware

--- middlewares/request.middleware.php
+++ middlewares/request.middleware.php
@@ -7,22 +7,23 @@ use App\SharedClasses\Objects\UserRequestObject;
 
 $endpoint = str_replace('/','',$_SERVER['REQUEST_URI']);
 
-$makeControllerName = "App\SharedClasses\Controller\\". ucwords($endpoint) . 'Controller';
+$makeControllerName = "App\SharedClasses\Controller\\". ucwords($endpoint) . 'Controller'; //Why double backward slashes here?
 
-$requestMethod = $_SERVER['REQUEST_METHOD'];
+$requestMethod = $_SERVER['REQUEST_METHOD']; // We know this to be post. Isn't it? Or we want to make it flexible?
 
 $method = 'index';
 
-$requestObject = "App\SharedClasses\Objects\Requests\\". ucwords($endpoint) . 'Request';
+$requestObject = "App\SharedClasses\Objects\Requests\\". ucwords($endpoint) . 'Request'; // is this not supposed to be 'use requestObject'
 
-$request = new $requestObject(
+
+$request = new $requestObject( // is this not supposed to be new RegisterRequest($args)
     method: MethodsEnum::{$requestMethod},
     user: getUserObject(),
     body: UserRequestObject::fromArray(extractRequestBody($requestMethod))
 );
 
 
-$controller = (new $makeControllerName($request))->{$method}();
+$controller = (new $makeControllerName($request))->{$method}(); // same for this? new RegisterController($args)
 
 //TODO: helper functions
 function extractRequestBody($request_method):array{
